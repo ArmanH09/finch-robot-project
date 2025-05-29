0a450ed9fe7da7d4ce6d7ea5f1b813839f6b083f
ArmanH09 <ahayrapet0004@mymail.lausd.net>
2025-05-29 10:33:12 -0700

    committed

--- src/main/java/com/Skib/test.java
+++ src/main/java/com/Skib/test.java
@@ -6,7 +6,7 @@ public class test {
     public static void main(String[] args) {
         Finch myFinch = new Finch();
 
-        myFinch.setTurn("L", 720, 100);
+        myFinch.setTurn("L", 7200, 100);
 
         myFinch.stopAll();
         myFinch.disconnect();
