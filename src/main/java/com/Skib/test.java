f158575eb64c07f3e0ddbabb68bfd46be80e5921
ArmanH09 <ahayrapet0004@mymail.lausd.net>
2025-06-02 09:41:47 -0700

    Update test.java

--- src/main/java/com/Skib/test.java
+++ src/main/java/com/Skib/test.java
@@ -5,9 +5,18 @@ import com.birdbrain.Finch;
 public class test {
     public static void main(String[] args) {
         Finch myFinch = new Finch();
+            
+        /* int[] arr = {1, 0, 1, 1, 1, 1, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 1, 1, 1, 1, 0, 1};
+         myFinch.setDisplay(arr); */
 
-        myFinch.setTurn("L", 720, 100);
+         String message = "JOSH";
 
+         String[] array = message.split("");
+        for (int i = 0; i < array.length; i++) {
+            myFinch.print(array[i]);
+            myFinch.pause(0.5);
+        }
+        myFinch.pause(2.0);
         myFinch.stopAll();
         myFinch.disconnect();
     }
